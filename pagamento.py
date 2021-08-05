81d4a40b891e231f276c8431a78029a930f8f849
maroberto <maroberto13@gmail.com>
2021-08-05 16:52:17 -0300

    descontos

--- pagamento.py
+++ pagamento.py
@@ -12,11 +12,20 @@ class Funcionario():
         total_descontos = (inss + vt + medico + odonto)
         if self.opcao == 1:
             pagamento = self.salario - total_descontos
-            print(f'Seu Pagamento com descontos R$: {pagamento:.2f}')
+            print(f'\nSeu Pagamento com descontos R$: {pagamento:.2f}')
         elif self.opcao == 2:
             pagamento = (self.salario - total_descontos) + add_noturno
-            print(f'Seu Pagamento com descontos R$: {pagamento:.2f}')
+            print(f'\nSeu Pagamento com descontos R$: {pagamento:.2f}')
 
+        agua = float(input('\nDigite o valor da àgua: '))
+        luz = float(input('Digite o valor da Luz: '))
+        internet = float(input('Digite o valor da Internet: '))
+        faculdade = float(input('Digite o valor da Faculdade: '))
+        total_despesas = (agua + luz + internet + faculdade)
+        saldo_receber = (pagamento - total_despesas)
+        print(f'\nO Total de despesas é R$: {total_despesas:.2f}')
+        print(f'\nSeu saldo de pagamento é R$: {saldo_receber:.2f}')
+    
     def Beneficios(self):
         escala_seis = 26
         escala_doze = 15
@@ -27,16 +36,16 @@ class Funcionario():
             valor_vr = escala_seis * vr_dia
             valor_vt = escala_seis * vt_dia
             print(
-                f'Sua escala é 6x1 com um total de {total_horas}h, receberá de VT: {valor_vt} e VR: {valor_vr:.2f}')
+                f'\nSua escala é 6x1 com um total de {total_horas}h, receberá de VT: {valor_vt} e VR: {valor_vr:.2f}')
         elif self.opcao == 2:
             total_horas = escala_doze * 12
             valor_vr = escala_doze * vr_dia
             valor_vt = escala_doze * vt_dia
             print(
-                f'Sua escala é 12x36 com um total de {total_horas}h, receberá de VT: {valor_vt} e VR: {valor_vr:.2f}')
+                f'\nSua escala é 12x36 com um total de {total_horas}h, receberá de VT: {valor_vt} e VR: {valor_vr:.2f}')
 
 
-func1 = Funcionario(input('Digite o valor do seu salário R$: '), input(
+func1 = Funcionario(input('\nDigite o valor do seu salário R$: '), input(
     'Digite a sua escala [1= 6x1 | 2= 12X36]: '))
 func1.Descontos()
 func1.Beneficios()
